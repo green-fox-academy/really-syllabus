4725750b44aa1e7cb4013c1ea778254b452f2859
gyorkibence <bence.gyorki@greenfoxacademy.com>
2018-11-08 14:26:27 +0100

    feat(atm): add users

--- CA/bankTransfer/atm.ts
+++ CA/bankTransfer/atm.ts
@@ -9,3 +9,10 @@
 // a withdraw, a checkhowmoney, and checkcash method
 
 // pls use exception handling, avoid code duplicates, and do everything with ES6 (if it's possible)
+
+const users = [
+  {name: "Béla", bankcardnumber: 1234123412341234, password: 1000, money: 1000},
+  {name: "Béci", bankcardnumber: 1234123412341235, password: 1001, money: 1000},
+  {name: "Cica", bankcardnumber: 1234123412341236, password: 1002, money: 1000},
+  {name: "Macska", bankcardnumber: 1234123412341237, password: 1003, money: 1000},
+];
