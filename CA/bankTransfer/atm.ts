07df004b50f7f0a2c51f1e48613bac159abdfec9
gyorkibence <bence.gyorki@greenfoxacademy.com>
2018-11-08 14:23:43 +0100

    feat(atm): add chore file

---
+++ CA/bankTransfer/atm.ts
@@ -0,0 +1,11 @@
+// WILKOMMEN INTO MÁJN ATM mesinó, bank transzferálló
+
+// project description:
+// Users has a name, bankcardnumber, password, money
+// and we have a bunch of users
+// ATM has a password, money, and a hasenoughMoney method
+// adminUser has a password, and a deposit method(he can only deposit to the ATM now)
+// userToDo has checkpassword method, a checkbankcardnumber method
+// a withdraw, a checkhowmoney, and checkcash method
+
+// pls use exception handling, avoid code duplicates, and do everything with ES6 (if it's possible)
